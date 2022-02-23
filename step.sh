d8a8c5ef4a81b4082c3f12d9ef2d68207ce998a0
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-02-23 12:10:22 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -3,20 +3,14 @@ set -ex
 
 # config
 final_app_name="$change_filename_build_name-v$change_filename_version_number-b$change_filename_build_number"
-final_app_name_aab="$final_app_name.aab"
-final_app_name_apk="$final_app_name.apk"
 
-new_aab_path="${BITRISE_DEPLOY_DIR}/$final_app_name_aab"
-new_apk_path="${BITRISE_DEPLOY_DIR}/$final_app_name_apk"
+new_build_path="${BITRISE_DEPLOY_DIR}/$final_app_name"
 
-echo "$new_aab_path"
-echo "$new_apk_path"
+# mv "$BITRISE_SIGNED_AAB_PATH" "$new_build_path.aab"
+# mv "$BITRISE_APK_PATH" "$new_build_path.apk"
 
-# mv "$BITRISE_SIGNED_AAB_PATH" "$new_aab_path"
-# mv "$BITRISE_APK_PATH" "$new_apk_path"
-
-envman add --key BITRISE_SIGNED_AAB_PATH --value "$new_aab_path"
-envman add --key BITRISE_APK_PATH --value "$new_apk_path"
-envman add --key BITRISE_APP_FINAL_NAME_AAB --value "$final_app_name_aab"
-envman add --key BITRISE_APP_FINAL_NAME_APK --value "$final_app_name_apk"
+envman add --key BITRISE_SIGNED_AAB_PATH --value "$new_build_path.aab"
+envman add --key BITRISE_APK_PATH --value "$new_build_path.apk"
+envman add --key BITRISE_APP_FINAL_NAME_AAB --value "$final_app_name.aab"
+envman add --key BITRISE_APP_FINAL_NAME_APK --value "$final_app_name.apk"
 envman add --key BITRISE_APP_FINAL_NAME --value "$final_app_name"
