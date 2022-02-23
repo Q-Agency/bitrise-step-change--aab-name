aca5107fa39ca9198fbc8a7322c03db9b1509d00
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-02-23 12:00:52 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,15 +2,15 @@
 set -ex
 
 # config
-final_app_name="${BUILD_NAME}-v${ANDROID_VERSION_NAME}-b${ANDROID_VERSION_CODE}"
-final_app_name_aab="${BUILD_NAME}-v${ANDROID_VERSION_NAME}-b${ANDROID_VERSION_CODE}.aab"
-final_app_name_apk="${BUILD_NAME}-v${ANDROID_VERSION_NAME}-b${ANDROID_VERSION_CODE}.apk"
+final_app_name="$change_filename_build_name-v$change_filename_version_number-b$change_filename_build_number"
+final_app_name_aab="$final_app_name.aab"
+final_app_name_apk="$final_app_name.apk"
 
 new_aab_path="${BITRISE_DEPLOY_DIR}/$final_app_name_aab"
 new_apk_path="${BITRISE_DEPLOY_DIR}/$final_app_name_apk"
 
-mv "$BITRISE_SIGNED_AAB_PATH" "$new_aab_path"
-mv "$BITRISE_APK_PATH" "$new_apk_path"
+# mv "$BITRISE_SIGNED_AAB_PATH" "$new_aab_path"
+# mv "$BITRISE_APK_PATH" "$new_apk_path"
 
 envman add --key BITRISE_SIGNED_AAB_PATH --value "$new_aab_path"
 envman add --key BITRISE_APK_PATH --value "$new_apk_path"
